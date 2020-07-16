3479891c642686c572d26ad0cf50883adafba8f4
meaghan jennings <meaghanljennings@gmail.com>
2020-07-16 11:41:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,13 @@ def batch_badge_creator(names)
 
 end
 
-def assign_rooms(speakers)
-  
+def assign_rooms(names)
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  puts "Hello, #{name}! You'll be assigned to room #{room}!"
   
 each_with_index
 
 
 badge_maker
+
+printer
