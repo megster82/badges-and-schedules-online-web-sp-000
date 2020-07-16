e829602e3c417d87e48c45730b56e04e8bcc1684
meaghan jennings <meaghanljennings@gmail.com>
2020-07-16 11:50:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,47 +4,23 @@ def badge_maker(name)
 return "Hello, my name is #{name}."  
 end
 
-def batch_badge_creator(names)
-
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+end 
+return array 
 end
 
 def assign_rooms(names)
-  puts "Hello, #{name}! You'll be assigned to room #{room}!"
-  
-each_with_index
-
-
-
-printer
-people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-
-
-def batch_badge_creator(array)
-  nuarray = []
+  new_array = []
+  room = 1
   array.each do |name|
-    nuarray.push("Hello, my name is #{name}.")
-  end
-  return nuarray
-end
-
-# batch_badge_creator(people)
-
-
-
-def assign_rooms(array)
-  nuarray = []
-  counter = 1
-  array.each do |name|
-    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
-  end
-  return nuarray
-end
+    new_array.push("Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1
+  end 
+  return new_array
+end 
 
 # assign_rooms(people)
 
